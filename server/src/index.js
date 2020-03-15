d2daafb585517c22f2d7d12605d34dc21f03939b
abizmo <abizmo@gmail.com>
2020-03-15 14:12:56 +0000

    Basic backend

---
+++ server/src/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello world!',
+  });
+});
+
+app.use('/api/logs', logs);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listen on http://localhost:${PORT}`);
+});
